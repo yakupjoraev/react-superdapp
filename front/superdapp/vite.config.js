e39792d95536f0c12b2f599f0dd305a1f15b891b
Yakup <yakupjoraev@gmail.com>
2023-12-30 14:58:59 +0300

    added new npm gh-pages

--- front/superdapp/vite.config.js
+++ front/superdapp/vite.config.js
@@ -4,7 +4,11 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '',
+  plugins: [
+    react(),
+  ],
+
   css: {
     postcss: {
       plugins: [
